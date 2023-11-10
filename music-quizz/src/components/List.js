8be2ff27d4861b21719b8e3fd4ad0541d88513f3
Chazzy11 <charlotterogers96@hotmail.com>
2023-11-10 14:51:52 +0000

    object rendering

--- music-quizz/src/components/List.js
+++ music-quizz/src/components/List.js
@@ -1,11 +1,11 @@
 
 
 export default function List({questions}) {
-    console.log(questions[0])
+    console.log(questions[0].question)
     return (
         <>
-        <p> {questions[0]}</p>
-        <p> {questions[1]}</p>
+        <p> {questions[0].question}</p>
+        <p> {questions[1].question}</p>
 
 
         </>
