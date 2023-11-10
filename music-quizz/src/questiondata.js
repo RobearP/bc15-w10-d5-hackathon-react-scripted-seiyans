ac63ba9b48534b25d7a833a29bfee9c76600f401
Chazzy11 <charlotterogers96@hotmail.com>
2023-11-10 12:30:10 +0000

    skeleton being created

---
+++ music-quizz/src/questiondata.js
@@ -0,0 +1,4 @@
+export const questionData = [ 
+    {id: 1, question: "How old is Taylor Swift?", wronganswer: "32", rightanswer: "33" }, 
+    {id: 2, question: "What year did all Slipknot members reveal their faces?", wronganswer: "2002", rightanswer: "2006"}
+]
