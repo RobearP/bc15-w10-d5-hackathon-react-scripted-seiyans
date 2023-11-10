2ee207e14767c90f719bd0e59653f2ece1789a26
RobearP <parobert1990@gmail.com>
2023-11-10 13:58:36 +0000

    Super Saiyan added

--- music-quizz/src/components/Footer.js
+++ music-quizz/src/components/Footer.js
@@ -1,10 +1,12 @@
  import Saiyan from '../images/Saiyan.png'
+ import SS from '../images/Super_Saiyan.png'
 
 export default function Footer() {
     return (
         <>
             <h2>Made by the Scripted Saiyans</h2>
         <img src={Saiyan} alt='Saiyan'/>
+        <img src={SS} alt='Super Saiyan'/>
         </>
     )
 }
